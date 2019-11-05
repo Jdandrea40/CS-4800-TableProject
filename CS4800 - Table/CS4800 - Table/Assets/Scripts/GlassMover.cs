302c4a47a73dbafc9ab2b33ac4f5ae70770829a7
Jdandrea40 <jdandrea40@yahoo.com>
2019-11-05 11:14:26 -0700

    Unzip

---
+++ CS4800 - Table/CS4800 - Table/Assets/Scripts/GlassMover.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassMover : MonoBehaviour
+{
+    [SerializeField] Transform[] glassMovePoints = new Transform[2];
+    Rigidbody2D rb2d;
+    bool goUp = false;
+    bool goDown = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            goDown = false;
+            goUp = true;
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            goUp = false;
+            goDown = true;
+        }
+
+        if (goUp)
+        {
+            StartCoroutine(Spin(1));
+            transform.position = Vector3.MoveTowards(transform.position, glassMovePoints[1].position, 1f);
+            if (transform.position.y > glassMovePoints[1].position.y + .01)
+            {
+                transform.position = glassMovePoints[1].position;
+            }
+        }
+
+        if (goDown)
+        {
+            StartCoroutine(Spin(-1));
+            transform.position = Vector3.MoveTowards(transform.position, glassMovePoints[0].position, 1f);
+            if (transform.position.y < glassMovePoints[0].position.y + 1)
+            {
+                transform.position = glassMovePoints[0].position;
+                transform.rotation = glassMovePoints[0].rotation;
+            }
+        }
+    }   
+    IEnumerator Spin(int direction)
+    {
+        if (transform.position != glassMovePoints[0].position)
+        {
+            transform.Rotate(0, 5 * direction, 0) ;
+        }
+            yield return null;
+    }
+}
---
+++ CS4800 - Table/CS4800 - Table/Assets/Scripts/PullForce.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullForce : MonoBehaviour
+{
+    [SerializeField] float Zstart;
+    Vector3 startPos;
+    SpringJoint sj;
+    Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        sj = GetComponent<SpringJoint>();
+        startPos = transform.position;
+        Zstart = transform.position.z;
+    }
+    private void Start()
+    {
+        
+        
+    }
+
+    private void OnMouseDrag()
+    {
+
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x,Input.mousePosition.y,15);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = objPosition;
+
+    }
+
+    IEnumerator ResetGrav()
+    {
+        yield return new WaitForSeconds(3);
+        rb.mass = 1;
+        rb.useGravity = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            transform.position = startPos;
+            rb.mass = 0;
+            rb.useGravity = false;
+            StartCoroutine(ResetGrav());
+        }
+    }
+}

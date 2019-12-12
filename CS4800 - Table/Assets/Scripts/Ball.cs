4f8f6b6baa907d2b0fab091435a42fff5c6d4eca
Jdandrea40 <jdandrea40@yahoo.com>
2019-12-12 10:48:27 -0700

    Assignment 3 finished?

---
+++ CS4800 - Table/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0, Random.Range(-100, -50), 0), ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        StartCoroutine(Die());
+    }
+
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
---
+++ CS4800 - Table/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(ball,new Vector3(transform.position.x + Random.Range(-1, 2), transform.position.y, transform.position.z + Random.Range(-1, 2)),Quaternion.identity);
+        }
+    }
+}
